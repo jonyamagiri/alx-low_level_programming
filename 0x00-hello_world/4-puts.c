3749e7abd2506b602646c45bfe6b51d7458170fe
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-06-02 15:02:06 +0000

    Add: Hello, puts script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints \"Programming is like building a multilingual puzzle\"
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
