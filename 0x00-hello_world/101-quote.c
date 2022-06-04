e6f113dff365273b9e8b7e6c6f13869ec86a6865
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-06-04 14:39:40 +0000

    Add: UNIX is basically a simple operating system script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
 /**
  *main - Entry point
  *
@@ -13,7 +11,7 @@
 
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	return (1);
+return (1);
 }
