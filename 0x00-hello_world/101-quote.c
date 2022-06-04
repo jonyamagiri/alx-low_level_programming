712a1a755479838906c3f7a200da4a4a6be812f2
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-06-04 11:20:41 +0000

    Add: UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
-
+#include <stdlib.h>
+#include <string.h>
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Program that prints to screen without use of printf and puts
+ *Description: Prints an error quote, followed by a new line,
+ *to the standard error
  *
- * Return: (1)
+ *Return: (1)
  */
-
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+
 	return (1);
 }
