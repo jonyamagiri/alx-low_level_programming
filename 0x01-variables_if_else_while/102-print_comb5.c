c63d93ef5e8526684f5abc120ea045180f5bfdfa
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-06-06 15:35:29 +0000

    Add: Software is eating the World script

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  *main - Entry point
+  *
+  *Description: Prints all possible combinations of two two-digit numbers
+  *
+  *Return: Always success (0)
+  *
+  */
+
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 57; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					if (a == 57 && a + b + c + d == 227)
+					{
+						break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
