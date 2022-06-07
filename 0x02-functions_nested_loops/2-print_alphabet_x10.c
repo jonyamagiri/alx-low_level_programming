f0a5137d9def521f23acea76dc4a908fa88b9d96
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-06-07 12:31:37 +0000

    Add: 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  *print_alphabet_x10 - Function that prints 10 times the alphabet,
+  *in lowercase, followed by a new line
+  *
+  *Return: void (0)
+  */
+
+void print_alphabet_x10(void)
+{
+	int alphab;
+	int i;
+
+	i = 0;
+	while (i <= 10)
+	{
+		alphab = 97;
+		while (alphab <= 122)
+		{
+			_putchar(alphab);
+			alphab++;
+		}
+	_putchar('\n');
+	i++;
+	}
+}
