9e9ba308f39a48afb9b964df084d79eb4de7a789
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-06-09 20:47:18 +0000

    Add: Fizz-Buzz program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *Description: Fizz-Buzz program for 1-100
+  *Return: Always success (0)
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((i % 3 == 0))
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5 == 0))
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
