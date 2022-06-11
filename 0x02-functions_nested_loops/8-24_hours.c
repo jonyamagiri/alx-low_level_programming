b5473b33fd42ad95cb17f49b9c5b85958684728d
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-06-11 03:41:44 -0700

    Add: agent Jack Bauer function

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,16 +12,17 @@ void jack_bauer(void)
 	int h, m;
 
 	h = 0;
-	while (h < 24)
+	while (h <= 23)
 	{
 		m = 0;
-		while (m < 60)
+		while (m <= 59)
 		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
 			_putchar(':');
 			_putchar((m / 10) + '0');
 			_putchar((m / 10) + '0');
+			_putchar('\n');
 			m++;
 		}
 		h++;
