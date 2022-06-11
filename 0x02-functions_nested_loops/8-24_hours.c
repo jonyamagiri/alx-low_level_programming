c8282e5407b13c0cfc92c35ff018e143c76f9624
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-06-11 04:24:39 -0700

    Add: Jack Bauer, and today is the longest day of my life function

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,27 +1,26 @@
-# include "main.h"
+#include "main.h"
 
 /**
-  *jack_bauer - Entry point
-  *@void: Does not take any arguments
-  *Description: prints every minute of the day of Jack Bauer
-  *Return: Void
-  */
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59
+ * Return: void
+ */
 
 void jack_bauer(void)
 {
 	int h, m;
 
 	h = 0;
-	while (h < 24)
+	while (h <= 23)
 	{
 		m = 0;
-		while (m < 60)
+		while (m <= 59)
 		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
 			_putchar(':');
 			_putchar((m / 10) + '0');
-			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar('\n');
 			m++;
 		}
