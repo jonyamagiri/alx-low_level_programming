c000395fa3e6b64f0417ea3cbab173de62182de7
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-06-17 10:46:38 +0000

    Add: function that prints an integer using putchar

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+# include "main.h"
+
+/**
+  * print_number - Entry point
+  * @n: integer value to be printed
+  * Description: Function prints an integer using _putchar()
+  * Return: void
+  */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n == 0)
+		_putchar('0');
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
