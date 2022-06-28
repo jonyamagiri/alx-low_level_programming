384aeb4f8ffaed1685c48d522f5555d70591c10a
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-06-28 14:12:56 +0000

    Add: function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,10 +10,10 @@
   * contains the concatenated string contents)
   */
 
-char *str_concat(char *s1, char*s2)
+char *str_concat(char *s1, char *s2)
 {
 	char *new_str;
-	int i = 0; new_i = 0; slen = 0;
+	int i = 0, new_i = 0, slen = 0;
 
 	if (s1 == NULL || s2 == NULL)
 	{
