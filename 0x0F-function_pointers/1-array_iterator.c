5cdda653acbd57df24ef9e8ec4d9899b40bc7236
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-07-06 21:20:59 +0300

    fix: correction to code

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,15 +15,13 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array == NULL && action == NULL)
+	if (array != NULL && action != NULL)
 	{
-		return;
-	}
-
-	i = 0;
-	while (i < size)
-	{
-		action(array[i]);
-		i++;
+		i = 0;
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
 	}
 }
