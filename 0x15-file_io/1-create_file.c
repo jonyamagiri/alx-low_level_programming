2faffc204bb77231f1622e63df554b1a34b129e3
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-07-25 19:29:13 +0300

    fixed requirement in function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, i = 0;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
