81439a882022815fa3c1e359c08e2194cf2b8c2e
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-07-25 19:34:41 +0300

    fixed loop and requirement for function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,27 +10,22 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i = 0;
+	int fd, writ, i = 0;
 
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content != NULL)
+	{
+		while (text_content[i])
+			i++;
+	}
+
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-	if (fd == -1)
-		return (-1);
+	writ = write(fd, text_content, i);
 
-	for (;text_content[i];)
-	{
-		i++;
-	}
-
-	if (text_content == NULL)
-	{
-		close(fd);
+	if (fd == 1 || writ == -1)
 		return (-1);
-	}
-	else
-		write(fd, text_content, i);
 
 	close(fd);
 	return (1);
