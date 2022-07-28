925fbf115e901c52b5f6f74ebe0d0b46c64dd74b
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-07-28 19:26:56 +0300

    corrected and locally tested

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,29 +1,19 @@
 # include "main.h"
 
 /**
-  * _strcmp - Entry point
-  * @s1: pointer to first string for comparison
-  * @s2: pointer to second string for comparison
-  * Description: Function that compares two strings
-  * Return: 0 (if s1 == s2), -ve int (if s1 < s2), +ve int (if s1 > s2)
-  */
+* _strcmp - Entry point
+* @s1: pointer to first string for comparison
+* @s2: pointer to second string for comparison
+* Description: Function that compares two strings
+* Return: 0 (if s1 == s2), -ve int (if s1 < s2), +ve int (if s1 > s2)
+*/
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int flag;
-
-	for (i = 0; (*(s1 + i)) != '\0'; i++)
+	while (*s1 && (*s1 == *s2))
 	{
-		if (*(s1 + i) != *(s2 + i))
-		{
-			flag = 0;
-			break;
-		}
+		s1++;
+		s2++;
 	}
-	if (flag)
-	{
-		return (0);
-	}
-	return (*s1 - *s2);
+	return (*(char *)s1 - *(char *)s2);
 }
