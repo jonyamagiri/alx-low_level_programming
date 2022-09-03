668a548379e1fead1c9e35f702daaf7cc2d5fc2a
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-09-03 10:04:38 +0300

    update header file

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,28 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/* standard libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Data structure */
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ * Description: doubly linked list node structure
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+/* Function prototypes */
+
+
+
+#endif /* LISTS_H */
