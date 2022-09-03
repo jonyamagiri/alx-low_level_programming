a2f8b6345bdd3bcd542d4eb4f35afc36cb9e1604
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-09-03 13:32:09 +0300

    fix deletes the node at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,29 +9,32 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int count;
-	dlistint_t *temp;
+	dlistint_t *temp = *head;
 
 	if (*head == NULL)
 		return (-1);
-	temp = *head;
-	if (index == 0)
+
+	while (index != 0)
 	{
-		*head = temp->next;
-		free(temp);
-		return (1);
+		if (temp == NULL)
+			return (-1);
+		temp = temp->next;
+		index--;
 	}
 
-	count = 0;
-	while (count < index)
+	if (temp == *head)
 	{
-		temp = temp->next;
-		if (temp->next == NULL)
-			return (-1);
-		count++;
+		*head = temp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
 	}
-	temp->prev->next = temp->next;
-	temp->next->prev = temp->prev;
+	else
+	{
+		temp->prev->next = temp->next;
+		if (temp->next != NULL)
+			temp->next->prev = temp->prev;
+	}
+
 	free(temp);
 
 	return (1);
