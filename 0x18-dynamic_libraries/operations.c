b7844c80fed0212bdf5213302abc83c61fd083c4
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-09-05 21:57:53 +0300

    dynamic library that contains C functions

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,59 @@
+/**
+* add - returns addition of two integers
+* @a: first integer
+* @b: second integer
+* Return: addition of two integers
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - returns subtraction of two integers
+* @a: first integer
+* @b: second integer
+* Return: subtraction of two integers
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* div - returns division of two integers
+* @a: first integer
+* @b: second integer
+* Return: division of two integers
+*/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mul - returns multiplication of two integers
+* @a: first integer
+* @b: second integer
+* Return: multiplication of two integers
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* mod - returns modulus of two integers
+* @a: first integer
+* @b: second integer
+* Return: modulus of two integers
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
