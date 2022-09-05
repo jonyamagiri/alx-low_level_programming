f03edbae809371b98aa7350052dae6bf480e119c
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-09-05 17:43:24 +0300

    fixed bug in script

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -fPIC -c *.c
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -shared -o liball.so *.o
-export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
