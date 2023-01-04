15b2c98bc34c3a678ae372d614e3122dfa8a6c44
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-01-04 21:36:53 +0300

    search_algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,18 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+
+/* standard functions */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* function prototypes */
+int linear_search(int *array, size_t size, int value);
+
+
+
+
+
+
+#endif /* SEARCH_ALGOS_H */
